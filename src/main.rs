91a0c457b7b57f4d5cb935b3e6990ca8a317e571
Amjad Alsharafi <amjadsharafi10@gmail.com>
2020-07-21 13:11:03 +0800

    Added hello world with some tests

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn addition() {
+        assert_eq!(2, 1 + 1);
+    }
+
+    #[test]
+    fn subtraction() {
+        assert_eq!(0, 1 - 1);
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
